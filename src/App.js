830b9a8e80f671c7c573acaec3c8a58013150cfb
Rafael Vieira <rafaelsvieira@outlook.pt>
2026-04-24 00:44:24 +0100

    Codigo alterado com sucesso

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'Rafael Vieira nº2022259';
+  return <div>Lab3 de  {value}</div>;
 }
 
 export default App;
